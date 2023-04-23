608dde97c610d704e1831bcde20d5ce0592d44fe
Charles Fettinger <charles@onacloud.org>
2023-04-23 02:56:24 -0700

    Refactor Update includes Implement Infinite Zoom Exit Image update gitignore specify seed new dataframe layout

--- iz_helpers/static_variables.py
+++ iz_helpers/static_variables.py
@@ -5,9 +5,9 @@ import modules.sd_samplers
 default_prompt = """
 {
     "prompts":{
-        "headers":["outpaint steps","prompt"],
+        "headers":["outpaint steps","prompt","image location"],
         "data":[
-            [0,"Huge spectacular Waterfall in a dense tropical forest,epic perspective,(vegetation overgrowth:1.3)(intricate, ornamentation:1.1),(baroque:1.1), fantasy, (realistic:1) digital painting , (magical,mystical:1.2) , (wide angle shot:1.4), (landscape composed:1.2)(medieval:1.1), divine,cinematic,(tropical forest:1.4),(river:1.3)mythology,india, volumetric lighting, Hindu ,epic,  Alex Horley Wenjun Lin greg rutkowski Ruan Jia (Wayne Barlowe:1.2) <lora:epiNoiseoffset_v2:0.6> "]
+            [0,"Huge spectacular Waterfall in a dense tropical forest,epic perspective,(vegetation overgrowth:1.3)(intricate, ornamentation:1.1),(baroque:1.1), fantasy, (realistic:1) digital painting , (magical,mystical:1.2) , (wide angle shot:1.4), (landscape composed:1.2)(medieval:1.1), divine,cinematic,(tropical forest:1.4),(river:1.3)mythology,india, volumetric lighting, Hindu ,epic,  Alex Horley Wenjun Lin greg rutkowski Ruan Jia (Wayne Barlowe:1.2) <lora:epiNoiseoffset_v2:0.6> ","C:\\path\\to\\image.png"]
         ]
     },
     "negPrompt":"frames, borderline, text, character, duplicate, error, out of frame, watermark, low quality, ugly, deformed, blur  bad-artist"
@@ -18,13 +18,13 @@ available_samplers = [
 ]
 
 empty_prompt = (
-    '{"prompts":{"data":[],"headers":["outpaint steps","prompt"]},"negPrompt":""}'
+    '{"prompts":{"data":[],"headers":["outpaint steps","prompt","image location"]},"negPrompt":""}'
 )
 
 invalid_prompt = {
     "prompts": {
-        "data": [[0, "Your prompt-json is invalid, please check Settings"]],
-        "headers": ["outpaint steps", "prompt"],
+        "data": [[0, "Your prompt-json is invalid, please check Settings",""]],
+        "headers": ["outpaint steps", "prompt","image location"],
     },
     "negPrompt": "Invalid prompt-json",
 }
