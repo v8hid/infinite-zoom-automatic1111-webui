e41d103791e394f17f7a029f347ece58f640beb8
vahid K. nejad <me@vahid.cloud>
2023-04-27 12:36:04 +0400

    fixing import

--- scripts/infinite-zoom.py
+++ scripts/infinite-zoom.py
@@ -1,7 +1,7 @@
 from modules import script_callbacks
-from iz_helpers import on_ui_tabs, on_ui_settings
+from iz_helpers.ui import on_ui_tabs
+from iz_helpers.settings import on_ui_settings
 
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
 script_callbacks.on_ui_settings(on_ui_settings)
-
