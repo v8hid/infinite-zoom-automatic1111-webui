2a92221ff01c7a0ced489e7ac7ae5e67b9c4da45
GeorgLegato <bulbul@onlinehome.de>
2023-05-01 01:29:11 +0200

    changed wording to help users understand concept very easily. disabling generate button on invalid table

--- iz_helpers/ui.py
+++ iz_helpers/ui.py
@@ -255,7 +255,7 @@ Our best experience and trade-off is the R-ERSGAn4x upscaler.
                 )
                 
         generate_btn.click(
-            fn=wrap_gradio_gpu_call(check_create_zoom, extra_outputs=[None, "", ""]),
+            fn=wrap_gradio_gpu_call(create_zoom, extra_outputs=[None, "", ""]),
             inputs=[
                 main_common_prompt_pre,
                 main_prompts,
