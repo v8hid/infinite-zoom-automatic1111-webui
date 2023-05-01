5cd909f3b314588735af29920d2708a33d244e2d
vahid K. nejad <me@vahid.cloud>
2023-05-01 16:29:32 +0400

    Printing path to saved video, typo fix

--- iz_helpers/run.py
+++ iz_helpers/run.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageFilter, ImageDraw
 from modules.ui import plaintext_to_html
 import modules.shared as shared
 from modules.processing import Processed, StableDiffusionProcessing
-
+from modules.paths_internal import script_path
 from .helpers import (
     fix_env_Path_ffprobe,
     closest_upper_divisible_by_eight,
@@ -221,7 +221,7 @@ def create_zoom(
 
 def prepare_output_path():
     isCollect = shared.opts.data.get("infzoom_collectAllResources", False)
-    output_path = shared.opts.data.get("infzoom_outpath", "output")
+    output_path = shared.opts.data.get("infzoom_outpath", "outputs")
 
     save_path = os.path.join(
         output_path, shared.opts.data.get("infzoom_outSUBpath", "infinite-zooms")
@@ -530,7 +530,7 @@ def create_zoom_single(
         int(video_start_frame_dupe_amount),
         int(video_last_frame_dupe_amount),
     )
-
+    print("Video saved in: " + os.path.join(script_path, out_config["video_filename"]))
     return (
         out_config["video_filename"],
         main_frames,
