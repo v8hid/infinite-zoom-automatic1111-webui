0b28b242003e407b85bd01ed7923f86366bb6523
ZZ <zzgigi2003@163.com>
2023-05-11 15:29:23 +0000

    Update install.py
    Signed-off-by: ZZ <zzgigi2003@163.com>

--- install.py
+++ install.py
@@ -2,5 +2,5 @@ import launch
 
 if not launch.is_installed("imageio"):
     launch.run_pip("install imageio", "requirements 0 for Infinite-Zoom")
-if not launch.is_installed("imageio-ffmpeg"):
+if not launch.is_installed("imageio_ffmpeg"):
     launch.run_pip("install imageio-ffmpeg", "requirements 1 for Infinite-Zoom")
